8cef9a580f0c3aa95858e3ec52883f07b6343186
S. Putivtsev <s.putivtsev@gmail.com>
2018-04-06 17:13:56 +0300

    initial project version

---
+++ src/command/ZipCreateCommand.java
@@ -0,0 +1,28 @@
+package command;
+
+import exception.PathIsNotFoundException;
+import helper.ConsoleHelper;
+import manager.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ZipCreateCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(sourcePath);
+
+            ConsoleHelper.writeMessage("Архив создан.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
+    }
+}
---
+++ src/operation/Operation.java
@@ -0,0 +1,10 @@
+package operation;
+
+public enum Operation {
+    CREATE,
+    ADD,
+    REMOVE,
+    EXTRACT,
+    CONTENT,
+    EXIT
+}
